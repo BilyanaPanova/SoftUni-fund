f89dee304f7460f615a1122cdb79dadd6fee3b32
Bilyana <156317102+BilyanaPanova@users.noreply.github.com>
2024-01-25 21:08:02 +0200

    Add files via upload

--- 04_josephus_permutation.py
+++ 04_josephus_permutation.py
@@ -1,16 +1,54 @@
-people_in_the_circle = [x for x in input().split(" ")]
-executed_person = int(input())
-
+people_in_the_circle = [int(x) for x in input().split(" ")]
+step = int(input())
 killed_people = []
-while len(people_in_the_circle) != 0:
-    index = executed_person - 1
-    if len(people_in_the_circle) <= index:
-        index = index % (len(people_in_the_circle))
-    killed_people.append(people_in_the_circle[index])
-    if len(people_in_the_circle) > 1:
-        people_in_the_circle = people_in_the_circle[index+1:] + people_in_the_circle[:index]
-    else:
-        del people_in_the_circle[index]
+index = step - 1
 
-result = ",".join(killed_people)
+while len(people_in_the_circle) != 0:
+    if index >= len(people_in_the_circle):
+        index -= len(people_in_the_circle)
+        continue
+
+    killed_people.append(people_in_the_circle[index])
+    del people_in_the_circle[index]
+    index += step - 1
+
+result = ",".join(map(str, killed_people))
 print(f"[{result}]")
+
+# 2
+
+# list_digit = [int(x) for x in input().split(" ")]
+# n = int(input())
+#
+# list_result = []
+# index = n - 1
+# current_index = index
+#
+# while len(list_digit) != 0:
+#     current_index = current_index % len(list_digit)
+#     list_result.append(list_digit[current_index])
+#     del list_digit[current_index]
+#     current_index += index
+# result = ",".join(map(str,list_result))
+# print(f"[{result}]")
+
+# 3 (от интернет!)
+
+# circle = input().split(' ')
+# kill_count = int(input())
+# result = []
+# counter = 0
+#
+# index = 0
+# while len(circle) > 0:
+#     counter += 1
+#
+#     if counter % kill_count == 0:
+#         result.append(circle.pop(index))
+#     else:
+#         index += 1
+#
+#     if index >= len(circle):
+#         index = 0
+#
+# print(str(result).replace(' ', '').replace('\'', ''))
